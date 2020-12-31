7cd0021cd24ef3706bb2db992ec8dfc826748e28
zqsh <zqsh19970218@gmail.com>
2020-12-30 19:37:01 -0500

    Save

---
+++ jamtorch/prototype/network.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+__all__ = ["MLP"]
+
+class MLP(nn.Module):
+    def __init__(self, input_size, n_hidden, hidden_size, output_size):
+        super().__init__()
+        layers = []
+        for _ in range(n_hidden):
+            layers.append(nn.Linear(input_size, hidden_size))
+            layers.append(nn.ReLU())
+            input_size = hidden_size
+        layers.append(nn.Linear(hidden_size, output_size))
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
