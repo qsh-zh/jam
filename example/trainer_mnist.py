de653d26d43bed16d1d716aa9544c897fb9b76e8
zqsh <zqsh19970218@gmail.com>
2020-12-31 23:55:16 -0500

    clean code and tiny

--- example/trainer_mnist.py
+++ example/trainer_mnist.py
@@ -8,8 +8,6 @@ from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 from jamtorch.trainer import Trainer
 from jammy.logging import Wandb
-
-# from jamtorch.meta import as_float
 from omegaconf import OmegaConf
 
 cfg = OmegaConf.load("mnist.yaml")
@@ -71,55 +69,6 @@ def epoch_after(trainer):
                 "it": trainer.iter_cnt,
             }
         )
-
-
-def train(args, model, device, train_loader, optimizer, epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print(
-                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
-                    epoch,
-                    batch_idx * len(data),
-                    len(train_loader.dataset),
-                    100.0 * batch_idx / len(train_loader),
-                    loss.item(),
-                )
-            )
-            if args.dry_run:
-                break
-
-
-def test(model, device, test_loader):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            # sum up batch loss
-            test_loss += F.nll_loss(output, target, reduction="sum").item()
-            # get the index of the max log-probability
-            pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-
-    print(
-        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
-            test_loss,
-            correct,
-            len(test_loader.dataset),
-            100.0 * correct / len(test_loader.dataset),
-        )
-    )
 
 
 def main():
@@ -215,13 +164,6 @@ def main():
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    # for epoch in range(1, args.epochs + 1):
-    #     train(args, model, device, train_loader, optimizer, epoch)
-    #     test(model, device, test_loader)
-    #     scheduler.step()
-
-    # if args.save_model:
-    #     torch.save(model.state_dict(), "mnist_cnn.pt")
 
     trainer = Trainer(model, optimizer, build_loss(device), scheduler)
     trainer.set_monitor(not args.debug)
