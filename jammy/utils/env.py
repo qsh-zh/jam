b5a8a7e2f7a80035ca10a40c7398e34f12992b06
zqsh <zqsh19970218@gmail.com>
2021-01-21 20:02:26 -0500

    add env dependency

---
+++ jammy/utils/env.py
@@ -0,0 +1,44 @@
+
+import os
+import sys
+from jammy.cli.keyboard import str2bool
+from .cache import cached_result
+
+__all__ = ['jam_getenv', 'jam_is_verbose', 'jam_is_debug']
+
+
+def jam_getenv(name, default=None, type=None, prefix=None):
+    if prefix is None:
+        prefix = 'JAM_'
+
+    value = os.getenv((prefix + name).upper(), default)
+
+    if value is None:
+        return None
+
+    if type is None:
+        return value
+    elif type == 'bool':
+        return str2bool(value)
+    else:
+        return type(value)
+
+
+@cached_result
+def jam_get_dashdebug_arg():
+    # Return True if there is a '-debug' or '--debug' arg in the argv.
+    for value in sys.argv:
+        if value in ('-debug', '--debug'):
+            return True
+    return False
+
+
+@cached_result
+def jam_is_verbose(default='n', prefix=None):
+    return jam_getenv('verbose', default, type='bool', prefix=prefix)
+
+
+@cached_result
+def jam_is_debug(default='n', prefix=None):
+    return jam_get_dashdebug_arg() or jam_getenv('debug', default, type='bool', prefix=prefix)
+
