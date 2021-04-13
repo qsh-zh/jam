2cc8520f9a98abd940359e0048febe0a0f3e4c63
zqsh <zqsh19970218@gmail.com>
2021-04-13 15:17:44 -0400

    adding get a batch sample

--- jamtorch/data/meta.py
+++ jamtorch/data/meta.py
@@ -1,5 +1,20 @@
+import torch
+from jammy.random.rng import gen_rng
 
 __all__ = ["get_batch"]
 
-def get_batch(data_loader):
-    return next(iter(data_loader))
+
+def get_batch_loader(data_loader):
+    return next(iter(data_loader))[0]
+
+
+def get_batch(data, size=10):
+    if isinstance(data, torch.utils.data.DataLoader):
+        return get_batch_loader(data)
+    elif isinstance(data, torch.utils.data.Dataset):
+        rng = gen_rng()
+        subset = torch.utils.data.Subset(data, rng.choice(len(data)), size=size)
+        loader = torch.utils.data.DataLoader(subset, batch_size=size)
+        return get_batch_loader(loader)
+    else:
+        raise RuntimeError
