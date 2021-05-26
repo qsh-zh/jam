0da725cd44a6c29786be08f8cb9a89e71f33b7ba
zqsh <zqsh19970218@gmail.com>
2021-05-26 15:51:14 -0400

    add color list default in matplotlib

--- jamviz/plt/color_list.py
+++ jamviz/plt/color_list.py
@@ -2,4 +2,6 @@ import matplotlib.pyplot as plt
 
 __all__ = ["jcolors"]
 
-jcolors = ["blue", "orange", "green", "red", "purple", "cyan", "brown"]
+# jcolors = ["blue", "orange", "green", "red", "purple", "cyan", "brown"]
+# https://stackoverflow.com/questions/42086276/get-default-line-colour-cycle
+jcolors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
