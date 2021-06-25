865a2d17f53b0103e44aaf1964288cbecc7c6c62
zqsh <zqsh19970218@gmail.com>
2021-06-25 17:24:41 -0400

    using yaml file

--- bin/_gen-rc.py
+++ bin/_gen-rc.py
@@ -1,15 +1,19 @@
+# pylint: disable=invalid-name
 import os
 import os.path as osp
 import sys
 import tempfile
+
 import yaml
+
 from jammy.logging import get_logger
 
-# logger = get_logger(sys.stdout, colorize=True, format="<green>{time}</green> <level>{message}</level>")
+# logger = get_logger(sys.stdout, colorize=True,
+#                   format="<green>{time}</green> <level>{message}</level>")
 logger = get_logger(sys.stderr, colorize=True, level="INFO")
 
 
-def load_system_settings(root, config, bash_file):
+def load_system_settings(root, config, bash_file):  # pylint: disable=unused-argument
     if "system" not in config or config["system"] is None:
         return
     envs = config["system"].get("envs", {})
@@ -18,7 +22,7 @@ def load_system_settings(root, config, bash_file):
         print("export {}={}".format(k, v), file=bash_file)
 
 
-def load_vendors(root, config, bash_file):
+def load_vendors(root, config, bash_file):  # pylint: disable=unused-argument
     if "vendors" not in config or config["vendors"] is None:
         return
 
@@ -32,7 +36,7 @@ def load_vendors(root, config, bash_file):
         )
 
 
-def load_conda_settings(root, config, bash_file):
+def load_conda_settings(root, config, bash_file):  # pylint: disable=unused-argument
     if "conda" not in config or config["conda"] is None:
         return
     target_env = config["conda"].get("env", "")
@@ -55,13 +59,13 @@ def load_yml_config(root, bash_file, recursive=False):
     if recursive:
         last_root = None
         while root != last_root:
-            yml_filename = osp.join(root, "jam_.yml")
+            yml_filename = osp.join(root, "jam_.yaml")
             if osp.isfile(yml_filename):
                 break
             last_root = root
             root = osp.dirname(root)
     else:
-        yml_filename = osp.join(root, "jam_.yml")
+        yml_filename = osp.join(root, "jam_.yaml")
 
     if osp.isfile(yml_filename):
         logger.critical("Loading jam config: {}.".format(osp.abspath(yml_filename)))
@@ -75,8 +79,10 @@ def load_yml_config(root, bash_file, recursive=False):
 
 def main():
     f = tempfile.NamedTemporaryFile("w", delete=False)
-    load_yml_config(osp.dirname(osp.dirname(__file__)), f) # jammy package configuration
-    load_yml_config(os.getcwd(), f, recursive=True) # project configuration
+    load_yml_config(
+        osp.dirname(osp.dirname(__file__)), f
+    )  # jammy package configuration
+    load_yml_config(os.getcwd(), f, recursive=True)  # project configuration
     f.close()
     print(f.name)
 
