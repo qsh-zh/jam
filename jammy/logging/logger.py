2b2ee1762a11df2dfd1e5b69c851548fb1a4aacc
zqsh <zqsh19970218@gmail.com>
2021-06-27 02:52:22 -0400

    good log-format

--- jammy/logging/logger.py
+++ jammy/logging/logger.py
@@ -7,6 +7,12 @@ __all__ = ["get_logger"]
 logger.remove()
 
 logger_sink = {}
+LOG_FORMAT = (
+    "<green>{time:MM-DD HH:mm:ss.SSS}</green> |"
+    "<red>[{process.name}]</red>|"
+    "<level>{level: <8}</level> |"
+    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+)
 
 
 def get_logger(file_name=None, clear=False, **kwargs):
@@ -18,6 +24,8 @@ def get_logger(file_name=None, clear=False, **kwargs):
         file_name = sys.stderr
         if "level" not in kwargs:
             kwargs["level"] = "INFO"
+    if "format" not in kwargs:
+        kwargs["format"] = LOG_FORMAT
     if file_name in logger_sink.values():
         # FIXME:
         # logger.debug("already registered")
@@ -27,4 +35,5 @@ def get_logger(file_name=None, clear=False, **kwargs):
         # kwargs["level"] = "DEBUG" if jam_is_debug() else "INFO"
         sink_id = logger.add(file_name, **kwargs)
         logger_sink[sink_id] = file_name
+        logger.debug(f"sink_id: {sink_id:02d} ---> {str(file_name)}")
     return logger
