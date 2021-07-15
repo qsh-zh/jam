f2f061272b25f853c38c66af7599da4b89add006
zqsh <zqsh19970218@gmail.com>
2021-07-14 23:33:35 -0400

    add cuda order

--- jammy/utils/gpu.py
+++ jammy/utils/gpu.py
@@ -15,6 +15,13 @@ def select_gpu():
     return query[least_utils_id].entry["index"]
 
 
+def gpu_by_util():
+    query = gpustat.new_query()
+    _, utils_list = get_memfree_util()
+    ids = np.argsort(utils_list)
+    return [query[id_item].entry["index"] for id_item in ids]
+
+
 def get_memfree_util():
     query = gpustat.new_query()
     free_space_list = [item.memory_free for item in query]
