4cfd868b0d153695a82677d45b70a45315853def
zqsh <zqsh19970218@gmail.com>
2021-09-18 23:03:15 -0400

    feat : adding get_jam_repo_git

--- jammy/__init__.py
+++ jammy/__init__.py
@@ -1,4 +1,4 @@
-import jammy.utils.git as git
+from jammy.utils import git
 from jammy.utils.env import jam_getenv
 from jammy.utils.init import init_main
 
@@ -40,3 +40,11 @@ if jam_getenv("IMPORT_ALL", "true", "bool"):
         synchronized,
         try_run,
     )
+
+
+def get_jam_repo_git():
+    """get git info running jammy
+
+    :return: (jam_sha, jam_diff)
+    """
+    return git.log_repo(__file__)
