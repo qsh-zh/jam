49ce231b4e391e8f7455baf48891f8ce25756b22
zqsh <zqsh19970218@gmail.com>
2021-10-07 00:30:30 -0400

    fix : EMA copy to model, use clean copy
    * copy weight instead of return model, because model can be augmented
      with modules

--- jamtorch/io/ema.py
+++ jamtorch/io/ema.py
@@ -1,7 +1,7 @@
 import copy
 
 import torch
-import torch.nn as nn
+from torch import nn
 
 from jamtorch.logging import get_logger
 
@@ -18,11 +18,11 @@ class EMA:
         self.cnt = 0
         if isinstance(model, nn.DataParallel):
             model = model.module
-        self._old_model = copy.deepcopy(model)
+        self.ema_delay_model = copy.deepcopy(model)
         self.forget_resume = forget_resume
 
     def forward(self, *args, **kwargs):
-        return self._old_model(*args, **kwargs)
+        return self.ema_delay_model(*args, **kwargs)
 
     def update_parameters(self, new_model):
         if hasattr(new_model, "module"):
@@ -33,7 +33,7 @@ class EMA:
         if self.model is None:
             self.model = new_model
         if self.cnt == self.num_warm:
-            self._old_model.load_state_dict(new_model.state_dict())
+            self.ema_delay_model.load_state_dict(new_model.state_dict())
             return
         with torch.no_grad():
             if self.cnt % self.num_every == 0:
@@ -47,19 +47,19 @@ class EMA:
 
     @property
     def model(self):
-        return self._old_model
+        return self.ema_delay_model
 
     @model.setter
     def model(self, model):
         if isinstance(model, nn.DataParallel):
             model = model.module
-        self._old_model = copy.deepcopy(model)
+        self.ema_delay_model = copy.deepcopy(model)
 
     def state_dict(self):
-        if self._old_model is None:
+        if self.ema_delay_model is None:
             model_params = None
         else:
-            model_params = self._old_model.state_dict()
+            model_params = self.ema_delay_model.state_dict()
         return {
             "cnt": self.cnt,
             "model": model_params,
@@ -69,15 +69,16 @@ class EMA:
         }
 
     def copy_to(self, model):  # pylint: disable= unused-argument
-        return self._old_model
+        # return self.ema_delay_model
+        model.load_state_dict(self.ema_delay_model.state_dict())
 
     def load_state_dict(self, ema_dict):
         logger = get_logger()
         self.cnt = ema_dict.get("cnt") or 0
         self.num_warm = ema_dict.get("num_warm") or 0
         if not self.forget_resume:
-            if self._old_model is not None and ema_dict.get("model") is not None:
-                self._old_model.load_state_dict(ema_dict.get("model"))
+            if self.ema_delay_model is not None and ema_dict.get("model") is not None:
+                self.ema_delay_model.load_state_dict(ema_dict.get("model"))
         self.beta = ema_dict.get("beta") or 0.9
         self.num_every = ema_dict.get("num_every") or 1
         logger.critical("=====> Loading EMA finish =====>")
